0ffabdd88ca18268fa7a76da2f681a29d4d59f79
Jeremy Huang <jhuan175@students.cs.ubc.ca>
2026-06-01 11:26:07 -0700

    Reccomendation now includes distance as a factor of consideration

--- src/services/recommendationService.ts
+++ src/services/recommendationService.ts
@@ -1,24 +1,54 @@
 import { pool } from "../db";
+import { haversineDistance } from "../utils/distance";
 
+export async function recommendStore(items: string[], userLat: number, userLon: number) {
+  const stores = await getStoreTotals(items);
+  
+  const scoredStores = stores.map(store => {
+    const distance = haversineDistance(userLat, userLon, store.lat, store.lng);
+
+    const score = calculateScore(Number(store.total_price), distance);
+
+    return {
+      ...store,
+      distance,
+      score
+    }
+  });
+
+  scoredStores.sort((a, b) => a.score - b.score);
+
+  console.log(scoredStores);
+
+  return scoredStores[0];
+}
 /*
  * Returns store for which the total price of the items is the lowest
  *
  * @param items - List of items that user wants to buy
  */
-export async function cheapestStore(items : string[]) {
+async function getStoreTotals(items : string[]) {
   const result = await pool.query(
     `SELECT 
       s.id,
       s.name,
+      s.lat,
+      s.lng,
       SUM(sp.price) AS total_price
     FROM store_prices sp
     JOIN stores s ON s.id = sp.store_id
     JOIN products p ON p.id = sp.product_id
     WHERE p.name = ANY($1)
-    GROUP BY s.id, s.name
-    ORDER BY total_price ASC
-    LIMIT 1;
+    GROUP BY s.id, s.name, s.lat, s.lng
+    ORDER BY total_price ASC;
     `, [items]);
 
-    return result.rows[0]
+    return result.rows;
+}
+
+
+function calculateScore(totalPrice: number, distance: number): number {
+  const distanceWeight = 0.2; // Weight for distance in the score (Each km equates to $0.20)
+
+  return totalPrice + distance * distanceWeight;
 }
