32e4c44013060c5a481020949e8796c3dcfb087c
Jeremy Huang <jhuan175@students.cs.ubc.ca>
2026-08-31 10:39:33 -0400

    second half

--- backend/seed.sql
+++ backend/seed.sql
@@ -19,24 +19,24 @@ INSERT INTO products (name) VALUES
 
 ('milk (2L)'),
 ('dozen (12) eggs'),
-('rice'),
+('rice'), -- todo
 ('white bread'),
-('chicken breast'),
+('chicken breast'), -- todo
 ('gala apples (3lb)'),
 ('bananas (1 bunch)'),
 ('pasta'),
 ('tomatoes (on the vine)'),
 ('cheese (500g)'),
-('potatoes'),
-('onions'),
-('carrots'),
-('cereal'),
-('butter'),
-('yogurt'),
-('ground beef'),
-('orange juice'),
-('coffee'),
-('flour');
+('yellow potatoes (10lb)'),
+('onions (3lb)'),
+('carrots (3lb)'),
+('cereal'), -- todo
+('butter (1lb)'), 
+('yogurt (650g)'),
+('ground beef (1lb)'),
+('orange juice (2.63L)'),
+('coffee'), -- todo
+('flour (2.5kg)');
 
 -- =========================
 -- STORE PRICES
@@ -53,7 +53,17 @@ INSERT INTO store_prices (store_id, product_id, price) VALUES
 (1, 7, 1.98),
 (1, 8, 2.00),
 (1, 9, 3.12),
-(1, 10, 6.45);
+(1, 10, 6.45),
+(1, 11, 7.99),
+(1, 12, 3.79),
+(1, 13, 8.99),
+(1, 14, 4.50),
+(1, 15, 4.50),
+(1, 16, 3.49),
+(1, 17, 8.50),
+(1, 18, 8.99),
+(1, 19, 3.12),
+(1, 20, 3.79);
 
 -- Safeway Davie Street (store_id = 2)
 INSERT INTO store_prices (store_id, product_id, price) VALUES
@@ -66,7 +76,17 @@ INSERT INTO store_prices (store_id, product_id, price) VALUES
 (2, 7, 2.25),
 (2, 8, 3.29),
 (2, 9, 5.47),
-(2, 10, 8.5);
+(2, 10, 8.5),
+(2, 11, 7.99),
+(2, 12, 5.49),
+(2, 13, 5.24),
+(2, 14, 8.29),
+(2, 15, 8.29),
+(2, 16, 5.49),
+(2, 17, 6.99),
+(2, 18, 11.99),
+(2, 19, 3.12),
+(2, 20, 4.99);
 
 -- Costco Downtown (store_id = 3)
 INSERT INTO store_prices (store_id, product_id, price) VALUES
@@ -79,7 +99,17 @@ INSERT INTO store_prices (store_id, product_id, price) VALUES
 (3, 7, 1.99),
 (3, 8, 2.69),
 (3, 9, 3.79),
-(3, 10, 5.89);
+(3, 10, 5.89),
+(3, 11, 7.99),
+(3, 12, 4.21),
+(3, 13, 8.99),
+(3, 14, 2.50),
+(3, 15, 12.49),
+(3, 16, 9.99),
+(3, 17, 1.98),
+(3, 18, 2.00),
+(3, 19, 3.12),
+(3, 20, 6.45);
 
 -- Save-On-Foods Olympic Village (store_id = 4)
 INSERT INTO store_prices (store_id, product_id, price) VALUES
@@ -92,7 +122,17 @@ INSERT INTO store_prices (store_id, product_id, price) VALUES
 (4, 7, 2.30),
 (4, 8, 3.50),
 (4, 9, 6.26),
-(4, 10, 6.65);
+(4, 10, 6.65),
+(4, 11, 9.99),
+(4, 12, 4.99),
+(4, 13, 5.49),
+(4, 14, 6.69),
+(4, 15, 6.69),
+(4, 16, 3.29),
+(4, 17, 8.50),
+(4, 18, 8.99),
+(4, 19, 6.26),
+(4, 20, 6.69);
 
 -- Walmart North Vancouver (store_id = 5)
 INSERT INTO store_prices (store_id, product_id, price) VALUES
@@ -105,4 +145,14 @@ INSERT INTO store_prices (store_id, product_id, price) VALUES
 (5, 7, 1.96),
 (5, 8, 2.47),
 (5, 9, 2.90),
-(5, 10, 6.25);
+(5, 10, 6.25),
+(5, 11, 7.97),
+(5, 12, 2.94),
+(5, 13, 2.94),
+(5, 14, 4.97),
+(5, 15, 4.97),
+(5, 16, 3.27),
+(5, 17, 7.98),
+(5, 18, 8.44),
+(5, 19, 2.90),
+(5, 20, 3.77);
